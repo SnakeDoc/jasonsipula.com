acf5a6daaee74b85c4110030cc43779c07e5a392
Jason Sipula <jason@bymbly.com>
2026-02-11 00:46:52 +0000

    stylelint config

---
+++ stylelint.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import("stylelint").Config} */
+export default {
+  extends: [
+    "stylelint-config-standard",
+    "@dreamsicle.io/stylelint-config-tailwindcss",
+    "stylelint-config-html",
+  ],
+};
